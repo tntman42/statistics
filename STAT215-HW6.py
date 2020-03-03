375c6461bebd6f5d8e5a8a1841c5d79b2c192ca8
tntman42 <tntman42@gmail.com>
2020-03-03 09:33:36 -0500

    Completed homework 6

--- STAT215-HW6.py
+++ STAT215-HW6.py
@@ -70,10 +70,10 @@ def problem_7():
 
 
 if __name__ == "__main__":
-    # problem_1()
-    # problem_2()
-    # problem_3()
-    # problem_5()
-    # problem_6()
+    problem_1()
+    problem_2()
+    problem_3()
+    problem_5()
+    problem_6()
     problem_7()
     quit()
